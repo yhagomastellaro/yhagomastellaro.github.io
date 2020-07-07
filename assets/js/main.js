312dd25fb12e4a6ea301972155c3f23b686c92ff
Yhago Mastellaro <ymmastellaro@gmail.com>
2020-07-07 18:15:58 -0300

    saving CSS choice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,28 @@
 var cssMode = document.getElementById('mainStyle').href;
+const cssModeStorage = localStorage.getItem('cssModeChosen');
+
+if(cssModeStorage){
+    if(cssModeStorage == 'dark'){
+        document.getElementById('mainStyle').href = 'assets/css/styles-dark.css';
+        document.getElementById('modeTheme').innerHTML = "LIGHT";
+    }else{
+        document.getElementById('mainStyle').href = 'assets/css/styles-light.css';
+        document.getElementById('modeTheme').innerHTML = "DARK";
+    }
+}
 
 function changeMode(){
     
     if(cssMode == 'assets/css/styles-light.css'){
         document.getElementById('mainStyle').href = 'assets/css/styles-dark.css';
         document.getElementById('modeTheme').innerHTML = "LIGHT";
-        cssMode = 'assets/css/styles-dark.css'
+        cssMode = 'assets/css/styles-dark.css';
+        localStorage.setItem('cssModeChosen', 'dark');
     }else{
         document.getElementById('mainStyle').href = 'assets/css/styles-light.css';
         document.getElementById('modeTheme').innerHTML = "DARK";
-        cssMode = 'assets/css/styles-light.css'
+        cssMode = 'assets/css/styles-light.css';
+        localStorage.setItem('cssModeChosen', 'light');
     }
-    
+
 }
