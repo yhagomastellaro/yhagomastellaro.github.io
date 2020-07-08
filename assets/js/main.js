6a880c2bf6cc12ae3d3a6dd9cedb17622b5b6edd
Yhago Mastellaro <ymmastellaro@gmail.com>
2020-07-08 18:33:31 -0300

    new form

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,33 +26,3 @@ function changeMode(){
     }
 
 }
-
-$(document).ready(function() {
-    $('#form').submit(function(e) {
-        e.preventDefault();
-        
-        const name = $('input[name="inputName"]').val();
-        const email = $('input[name="inputEmail"]').val();
-        const subject = $('input[name="inputSubject"]').val();
-        const message = $('input[name="inputMessage"]').val();
-
-        $.ajax({
-            url: '../assets/php/sendMail.php', // caminho para o script que vai processar os dados
-            type: 'POST',
-            data: {inputName: name, inputEmail: email, inputSubject: subject, inputMessage: message},
-            success: function(response) {
-                document.getElementById('inputName').value='';
-                document.getElementById('inputEmail').value='';
-                document.getElementById('inputSubject').value='';
-                document.getElementById('inputMessage').value='';
-                setTimeout(function(){
-                    $('#form-success').show( "slow" );
-                }, 300)
-            },
-            error: function(xhr, status, error) {
-                alert(xhr.responseText);
-            }
-        });
-        return false;
-    });
- });
